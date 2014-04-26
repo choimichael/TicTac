bff599d57239b43a0e4dd97c6bdc9f9b7068fd28
Choi.Michael <typeinmichael@gmail.com>
2014-04-25 19:31:16 -0700

    Almost done :)

--- script.js
+++ script.js
@@ -1,117 +1,191 @@
-var gameApp = angular.module('gameApp', []);
-gameApp.controller('TicTacController', function($scope){
+var blankBoard = [[0,0,0],[0,0,0],[0,0,0]];
+var gameApp = angular.module('gameApp', ["firebase"]); 
+gameApp.controller('TicTacController', function($scope, $firebase){ //Dependency Injection
 
-	$scope.countX = 0;
-	$scope.countO = 0;
+	// // Track if we're player 1 or 2
+var playerNum = null;
 
-	var gameSt = true;
+		var ticTacRef = new Firebase("https://kotic.firebaseio.com/games");
+			var lastGame;
+			// Ask for all existing game info from firebase
+			ticTacRef.once('value', function(gamesSnapshot) {
+				// get the actual games data
+			  var games = gamesSnapshot.val();
+				if(games == null)
+				{
+					// No games at all, so make a new game -- As if we're Areg
+					lastGame = ticTacRef.push( {waiting: true} );
+					playerNum = 1;
+				}
+				else	// I do have at least one game out there...
+				{
+				  var keys = Object.keys(games);
+				  var lastGameKey = keys[ keys.length - 1 ];
+				  var lastGame = games[ lastGameKey ];
+					console.log("This person's game: " + lastGameKey);
+				  if(lastGame.waiting)
+				  {
+				  	// Currently someone is waiting -- Areg is there and we're Rocky
+				  	// Grab from Firebase its last game object
+				  	lastGame = ticTacRef.child(lastGameKey);
+				  	// Set a new game on this
+				  	lastGame.set( {
+				  		countX: 0, 
+				  		countO: 0, 
+				  		waiting:false, 
+				  		moveCount: 0, 
+				  		win: false, 
+				  		rows: blankBoard,
+				  		turn: true
+				  	} );
+				  	console.log(lastGame.set);
+				  	playerNum = false;
+				  }
+				  else
+				  {
+				  	// Make a new game -- As if we're Areg
+						lastGame = ticTacRef.push( {waiting: true} );
+						playerNum = true;
+				  }
+				
+				// Attach the last game to what we're up to
+			  $scope.game = $firebase(lastGame); //translating to JS from Firebase
+			
+
+		};
+	});
+	
+
+	var win = false;
+	var moveCount = 0;
 	var turn = true;
 
 	$scope.playerTurn = function(r, c)
 	{
-		//if ($scope.gameSt == 0){
-			//console.log($scope.gameSt == 0)
 		
-		if($scope.rows[r][c] != 0)
+		if($scope.game.rows[r][c] != 0 || playerNum != $scope.game.turn)
 		{
+			console.log(playerNum);
 			return;
 		}
-		else if(turn === true)
+		else if($scope.game.turn === true )
 		{
-			$scope.rows[r][c] = 1;	// X
-			turn = false;
-			//turnCount++;
+			$scope.game.rows[r][c] = 1;	// X
+			$scope.game.turn = false;
+			moveCount++;
+			console.log(moveCount);
 		}
 		else
 		{
-			$scope.rows[r][c] = -1;	// O
-			turn = true;
+			$scope.game.rows[r][c] = -1;	// O
+			$scope.game.turn = true;
+			moveCount++;
+			console.log(moveCount);
 		}
+		$scope.game.$save(); 
 
 		// Test various win conditions
-		if(Math.abs($scope.rows[0][0] + $scope.rows[0][1] + $scope.rows[0][2]) == 3){
-			$scope.playerWon("top row", $scope.rows[0][0])
+		if(Math.abs($scope.game.rows[0][0] + $scope.game.rows[0][1] + $scope.game.rows[0][2]) == 3){
+			win = true;
+			$scope.playerWon("top row", $scope.game.rows[0][0]);
 				
-				if($scope.rows[0][0] == 1) {
-					$scope.countX++;
+				if($scope.game.rows[0][0] == 1){
+					$scope.game.countX++;
 				}else{
-					$scope.countO++;
+					$scope.game.countO++;
+				}
+				console.log($scope.game.rows[0][0] == 1);
+			};
+		
+		if(Math.abs($scope.game.rows[1][0] + $scope.game.rows[1][1] + $scope.game.rows[1][2]) == 3){
+			win = true;
+			$scope.playerWon("middle row", $scope.game.rows[1][2]);
+
+				if($scope.game.rows[1][2] == 1){
+					$scope.game.countX++;
+				}else{
+					$scope.game.countO++;
 				} 
 			};
 		
-		if(Math.abs($scope.rows[1][0] + $scope.rows[1][1] + $scope.rows[1][2]) == 3){
-			$scope.playerWon("middle row", $scope.rows[1][2]);
+		if(Math.abs($scope.game.rows[2][0] + $scope.game.rows[2][1] + $scope.game.rows[2][2]) == 3){
+			win = true;
+			$scope.playerWon("bottom row", $scope.game.rows[2][0]);
 
-				if($scope.rows[1][2] == 1) {
-					$scope.countX++;
+				if($scope.game.rows[2][0] == 1){
+					$scope.game.countX++;
 				}else{
-					$scope.countO++;
+					$scope.game.countO++;
 				} 
 			};
 		
-		if(Math.abs($scope.rows[2][0] + $scope.rows[2][1] + $scope.rows[2][2]) == 3){
-			$scope.playerWon("bottom row", $scope.rows[2][0]);
+		if(Math.abs($scope.game.rows[0][0] + $scope.game.rows[1][1] + $scope.game.rows[2][2]) == 3){
+			win = true;
+			$scope.playerWon("\\", $scope.game.rows[1][1]);
 
-				if($scope.rows[2][0] == 1) {
-					$scope.countX++;
+				if($scope.game.rows[1][1] == 1){
+					$scope.game.countX++;
 				}else{
-					$scope.countO++;
+					$scope.game.countO++;
 				} 
 			};
 		
-		if(Math.abs($scope.rows[0][0] + $scope.rows[1][1] + $scope.rows[2][2]) == 3){
-			$scope.playerWon("\\", $scope.rows[1][1]);
+		if(Math.abs($scope.game.rows[0][2] + $scope.game.rows[1][1] + $scope.game.rows[2][0]) == 3){
+			win = true;
+			$scope.playerWon("/", $scope.game.rows[2][0]);
 
-				if($scope.rows[1][1] == 1) {
-					$scope.countX++;
+				if($scope.game.rows[2][0] == 1){
+					$scope.game.countX++;
 				}else{
-					$scope.countO++;
+					$scope.game.countO++;
 				} 
 			};
 		
-		if(Math.abs($scope.rows[0][2] + $scope.rows[1][1] + $scope.rows[2][0]) == 3){
-			$scope.playerWon("/", $scope.rows[2][0]);
+		if(Math.abs($scope.game.rows[0][0] + $scope.game.rows[1][0] + $scope.game.rows[2][0]) == 3){
+			win = true;
+			$scope.playerWon("right |", $scope.game.rows[1][0]);
 
-				if($scope.rows[2][0] == 1) {
-					$scope.countX++;
+				if($scope.game.rows[1][0] == 1){
+					$scope.game.countX++;
 				}else{
-					$scope.countO++;
-				} 
-			};
-		
-		if(Math.abs($scope.rows[0][0] + $scope.rows[1][0] + $scope.rows[2][0]) == 3){
-			$scope.playerWon("right |", $scope.rows[1][0]);
-
-				if($scope.rows[1][0] == 1) {
-					$scope.countX++;
-				}else{
-					$scope.countO++;
+					$scope.game.countO++;
 				} 
 			};
 
-		if(Math.abs($scope.rows[0][2] + $scope.rows[1][2] + $scope.rows[2][2]) == 3){
-			$scope.playerWon("left |", $scope.rows[2][2]);
+		if(Math.abs($scope.game.rows[0][2] + $scope.game.rows[1][2] + $scope.game.rows[2][2]) == 3){
+			win = true;
+			$scope.playerWon("left |", $scope.game.rows[2][2]);
 		
-				if($scope.rows[0][0] == 1) {
-					$scope.countX++;
+				if($scope.game.rows[0][0] == 1){
+					$scope.game.countX++;
 				}else{
-					$scope.countO++;
+					$scope.game.countO++;
 				} 
 
 			};
 		
-		if(Math.abs($scope.rows[0][1] + $scope.rows[1][1] + $scope.rows[2][1]) == 3){
-			$scope.playerWon("middle |", $scope.rows[2][1]);
+		if(Math.abs($scope.game.rows[0][1] + $scope.game.rows[1][1] + $scope.game.rows[2][1]) == 3){
+			win = true;
+			$scope.playerWon("middle |", $scope.game.rows[2][1]);
 			
-			if($scope.rows[0][0] == 1) {
-					$scope.countX++;
+			if($scope.game.rows[0][0] == 1){
+					$scope.game.countX++;
 				}else{
-					$scope.countO++;
-				} 
-		};
+					$scope.game.countO++;
+				}
+			};
 		
+		if(moveCount == 9 && win === false){
+ 			console.log("It's a tie!");
+ 			return;
+ 		}
 
-		//if (turnCount == 9)
+ 		// // no win, keep going
+ 		// else {
+ 		// 	// nothing
+ 		// }
+
+			
 	
 };
 	// Show an alert if one of the tested directions is a win
@@ -121,16 +195,18 @@ gameApp.controller('TicTacController', function($scope){
 		alert( ((player == 1)?"X" : "O") + " won by " + descrip);
 
 	}
-
+	 	//Play Again button without loosing the scores
 	$scope.playAg = function(){
-		$scope.rows = [ [0,0,0],[0,0,0],[0,0,0] ];
+		$scope.game.rows = blankBoard;
+		$scope.game.moveCount = 0;
+		$scope.game.win = false;
 		
 	};
-
+		//Reset the entire game
 	$scope.reset = function(){
-		$scope.rows = [ [0,0,0],[0,0,0],[0,0,0] ];
-		$scope.countX = 0;
-		$scope.countO = 0;
+		$scope.game.rows = blankBoard;
+		$scope.game.countX = 0;
+		$scope.game.countO = 0;
 	};
 
 });
